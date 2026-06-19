4d0dbf37b9b03a772ee532dadb52d80ef8cf89c1
Tarunnnnnn <Tarunsiddhantsiddhant@gmail.com>
2026-06-20 03:04:43 +0530

    Transparent navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from 'react'
 import './Navbar.css'
 
-function Navbar(){
-    return(
-        <nav className="navbar">
-            <p className="navbar__logo">TOUR ME</p>
-            <div className="navbar__links">
-                <a href="#">Explore</a>
-                <a href="#">About</a>
-            </div>
-        </nav>
-    )
+function Navbar() {
+  const [solid, setSolid] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const heroHeight = document.querySelector('.hero')?.offsetHeight || 0
+      setSolid(window.scrollY < heroHeight)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <nav className={`navbar ${solid ? 'navbar--solid' : ''}`}>
+      <p className="navbar__logo">TOUR ME</p>
+      <div className="navbar__links">
+        <a href="#">Explore</a>
+        <a href="#">About</a>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
